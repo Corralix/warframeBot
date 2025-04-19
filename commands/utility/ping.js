3d42b127573c7e07004445a93fc7fd49ea63626d
RabjamX2 <110269492+RabjamX2@users.noreply.github.com>
2025-04-19 18:09:04 -0400

    embed test

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,28 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js"); // https://discordjs.guide/popular-topics/embeds.html#embed-preview
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+	data: new SlashCommandBuilder().setName("emb").setDescription("Embed example"),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		// inside a command, event listener, etc.
+		const exampleEmbed = new EmbedBuilder()
+			.setColor(0x0099ff)
+			.setTitle("Some title")
+			.setURL("https://discord.js.org/")
+			.setAuthor({ name: "Some name", iconURL: "https://i.imgur.com/AfFp7pu.png", url: "https://discord.js.org" })
+			.setDescription("Some description here")
+			.setThumbnail("https://i.imgur.com/AfFp7pu.png")
+			.addFields(
+				{ name: "Regular field title", value: "Some value here" },
+				{ name: "\u200B", value: "\u200B" },
+				{ name: "Inline field title", value: "Some value here", inline: true },
+				{ name: "Inline field title", value: "Some value here", inline: true }
+			)
+			.addFields({ name: "Inline field title", value: "Some value here", inline: true })
+			.setImage("https://i.imgur.com/AfFp7pu.png")
+			.setTimestamp()
+			.setFooter({ text: "Some footer text here", iconURL: "https://i.imgur.com/AfFp7pu.png" });
+
+		// channel.send({ embeds: [exampleEmbed] });
+		await interaction.reply({ embeds: [exampleEmbed] });
 	},
 };
