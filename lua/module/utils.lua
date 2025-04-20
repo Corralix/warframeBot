96ef0f2f37769f623715f7f354c512fa8d93e8b8
RabjamX2 <110269492+RabjamX2@users.noreply.github.com>
2025-04-20 11:02:30 -0400

    added type checking

--- lua/module/utils.lua
+++ lua/module/utils.lua
@@ -3,9 +3,9 @@
 local H = {}
 
 --- Checks if a table contains a specific value.
--- @param table The table to search.
--- @param value The value to look for.
--- @return boolean True if the value is found in the table, false otherwise.
+--- @param table table The table to search.
+--- @param value string The value to look for.
+--- @return boolean boolean if the value is found in the table, false otherwise.
 function H.tableContains(table, value)
     for i = 1, #table do
         if (table[i] == value) then
@@ -16,9 +16,9 @@ function H.tableContains(table, value)
 end
 
 --- Concatenates the contents of one table to another.
--- @param t1 The first table to which the contents of the second table will be added.
--- @param t2 The second table whose contents will be appended to the first table.
--- @return table The modified first table with the contents of the second table appended.
+--- @param t1 table The first table to which the contents of the second table will be added.
+--- @param t2 table The second table whose contents will be appended to the first table.
+--- @return table wholeTable modified first table with the contents of the second table appended.
 function H.tableConcat(t1, t2)
     for i = 1, #t2 do
         t1[#t1 + 1] = t2[i]
@@ -27,9 +27,9 @@ function H.tableConcat(t1, t2)
 end
 
 --- Writes a string to a file in a specified directory with a given name.
--- @param stringToWrite The string to write to the file.
--- @param fileName The name of the file (without extension).
--- @param fileDirectory The directory where the file will be created.
+--- @param stringToWrite string The string to write to the file.
+--- @param fileName string The name of the file (without extension).
+--- @param fileDirectory string The directory where the file will be created.
 function H.stringToFile(stringToWrite, fileName, fileDirectory)
     local filePath = fileDirectory .. fileName .. ".json"
     local file = io.open(filePath, "w")
