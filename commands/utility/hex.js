cdc44b12db8d4f487ac65e6d346404283e9f0d2d
Corralix <edwardliao.el@gmail.com>
2025-04-20 20:30:22 -0400

    Implemented all bounty commands

--- commands/utility/hex.js
+++ commands/utility/hex.js
@@ -1,26 +1,78 @@
 const { fetchData } = require('../../modules/webSnatcher.mjs');
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const MissionDetails = require('../../json/MissionDetails.json');
+const Challenges = require('../../json/ExportChallenges.json');
+const wfDict = require('../../json/wfDict.json');
 
-// https://browse.wf/warframe-public-export-plus/dict.en.json
+const commandDetails = {
+    commandName: "hex",
+    description: "Delivers all Hex Bounties",
+    bountyId: "HexSyndicate",
+    syndicateNameString: "The Hex"
+}
+
+
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('hex')
-        .setDescription('Delivers all Hex Bounties'),
+        .setName(commandDetails.commandName)
+        .setDescription(commandDetails.description),
+        
     async execute(interaction) {
         fetchData('https://oracle.browse.wf/bounty-cycle')
         .then(data => {
-            let Output = "The Hex:\n";
-            let bounty = data["bounties"]["HexSyndicate"];
+            let time = Math.floor(data["expiry"] / 1000);
+
+            let timeString = `Expires at: <t:${time}:t> (<t:${time}:R>)`;
+            
+
+            let bounty = data["bounties"][commandDetails.bountyId];
+            console.log(bounty);
+
+            let nodeList = [];
+            let challengeList = [];
         
             for (let rank = 0; rank < bounty.length; rank++) {
+                let bountyRank = bounty[rank];
 
-                let node = MissionDetails[bounty[rank]["node"]];
+                let node = MissionDetails[bountyRank["node"]];
 
-                Output += `${rank+1}: ${node.Name}, ${bounty[rank]["challenge"]} \n`;
+                let challengePath = Challenges[bountyRank["challenge"]];
+                let challengeDesc = challengePath["description"];
+                let challengeCount = challengePath["requiredCount"];
+                let challengeTranslation = wfDict[challengeDesc];
+                challengeTranslation = challengeTranslation.replace("|COUNT|", `${challengeCount}`);
+
+                nodeList.push(node)
+                challengeList.push(challengeTranslation);
+
             }
-        
-            interaction.reply(Output);
+
+            
+            function fieldGenerator(nodeList, challengeList) {
+                let output = "";
+                let rankEmoji = ["\u0031\uFE0F\u20E3", "\u0032\uFE0F\u20E3", "\u0033\uFE0F\u20E3", "\u0034\uFE0F\u20E3", "\u0035\uFE0F\u20E3"];
+
+                for (let i = 0; i < nodeList.length; i++) {
+                    output += (`${rankEmoji[i]} **${nodeList[i]["Name"]}** (${nodeList[i]["Type"]})\n`)
+                    output += (`${challengeList[i]}\n\n`);
+                }
+
+                return { name: "", value: output}
+            }
+
+            const exampleEmbed = new EmbedBuilder()
+                        .setColor(0x0099ff)
+                        .setTitle(commandDetails.syndicateNameString)
+                        .setAuthor({ name: "ada", iconURL: "https://i.imgur.com/AfFp7pu.png", url: "https://browse.wf/about" })
+                        .setDescription(timeString)
+                        .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+                        .addFields(
+                            fieldGenerator(nodeList, challengeList)
+                        )
+                        .setFooter({ text: "Lilypad 🧑‍🌾"})
+                        .setTimestamp();
+
+            interaction.reply({ embeds: [exampleEmbed] });
         }
         )
         .catch(error => {
--- commands/utility/zariman.js
+++ commands/utility/zariman.js
@@ -1,47 +1,78 @@
 const { fetchData } = require('../../modules/webSnatcher.mjs');
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const MissionDetails = require('../../json/MissionDetails.json');
+const Challenges = require('../../json/ExportChallenges.json');
+const wfDict = require('../../json/wfDict.json');
 
-// async function bounty() {
-//     fetchData('https://oracle.browse.wf/bounty-cycle')
-//         .then(data => {
-//             Output = "Zariman Bounties:\n"
-//             bounty = data["bounties"]["ZarimanSyndicate"]
-        
-//             for (let rank = 0; rank < bounty.length; rank++) {
-//                 Output += `${rank+1}: ${bounty[rank]["node"]}, ${bounty[rank]["challenge"]} \n`
-//             }
-        
-//             return Output;
-//         }
-//         )
-//         .catch(error => {
-//             console.error('There was a problem with the fetch operation:', error);
-//         });
-// }
+const commandDetails = {
+    name: "zariman",
+    description: "Delivers all Zariman Bounties",
+    bountyId: "ZarimanSyndicate",
+    syndicateNameString: "The Holdfasts"
+}
 
 
-// https://browse.wf/warframe-public-export-plus/dict.en.json
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('zariman')
-        .setDescription('Delivers all Zariman Bounties'),
+        .setName(commandDetails.name)
+        .setDescription(commandDetails.description),
+        
     async execute(interaction) {
         fetchData('https://oracle.browse.wf/bounty-cycle')
         .then(data => {
-            let time = data["expiry"];
-            let faction = data["zarimanFaction"];
-            let Output = `Expires in: <t:${time}:t>\n##The Holdfasts:\n###Faction: ${faction}`;
-            let bounty = data["bounties"]["ZarimanSyndicate"];
+            let time = Math.floor(data["expiry"] / 1000);
+            let faction = data["zarimanFaction"].slice(3);
+
+            let timeString = `Expires at: <t:${time}:t> (<t:${time}:R>)`;
+            let factionString = `${faction}`;
+
+            let bounty = data["bounties"][commandDetails.bountyId];
+
+            let nodeList = [];
+            let challengeList = [];
         
             for (let rank = 0; rank < bounty.length; rank++) {
+                let bountyRank = bounty[rank];
 
-                let node = MissionDetails[bounty[rank]["node"]];
+                let node = MissionDetails[bountyRank["node"]];
 
-                Output += `${rank+1}: ${node.Name}, ${bounty[rank]["challenge"]} \n`;
+                let challengePath = Challenges[bountyRank["challenge"]];
+                let challengeDesc = challengePath["description"];
+                let challengeCount = challengePath["requiredCount"];
+                let challengeTranslation = wfDict[challengeDesc];
+                challengeTranslation = challengeTranslation.replace("|COUNT|", `${challengeCount}`);
+
+                nodeList.push(node)
+                challengeList.push(challengeTranslation);
+
             }
-        
-            interaction.reply(Output);
+
+            
+            function fieldGenerator(nodeList, challengeList) {
+                let output = "";
+                let rankEmoji = ["\u0031\uFE0F\u20E3", "\u0032\uFE0F\u20E3", "\u0033\uFE0F\u20E3", "\u0034\uFE0F\u20E3", "\u0035\uFE0F\u20E3"];
+
+                for (let i = 0; i < nodeList.length; i++) {
+                    output += (`${rankEmoji[i]} **${nodeList[i]["Name"]}** (${nodeList[i]["Type"]})\n`)
+                    output += (`${challengeList[i]}\n\n`);
+                }
+
+                return { name: "", value: output}
+            }
+
+            const exampleEmbed = new EmbedBuilder()
+                        .setColor(0x0099ff)
+                        .setTitle(commandDetails.syndicateNameString)
+                        .setAuthor({ name: `${factionString}`, iconURL: "https://i.imgur.com/AfFp7pu.png", url: "https://browse.wf/about" })
+                        .setDescription(timeString)
+                        .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+                        .addFields(
+                            fieldGenerator(nodeList, challengeList)
+                        )
+                        .setFooter({ text: "Lilypad 🧑‍🌾"})
+                        .setTimestamp();
+
+            interaction.reply({ embeds: [exampleEmbed] });
         }
         )
         .catch(error => {
